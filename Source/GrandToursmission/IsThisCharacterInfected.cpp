473895b45dc047ccd84b2bbbb2956f1e47d78aae
Jourdan-Reiss Russell <jourdan.reiss@gmail.com>
2018-01-27 15:42:15 +0000

    NPC added
    follows character on sight and if infected

---
+++ Source/GrandToursmission/IsThisCharacterInfected.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IsThisCharacterInfected.h"
+
+IsThisCharacterInfected::IsThisCharacterInfected()
+{
+}
+
+IsThisCharacterInfected::~IsThisCharacterInfected()
+{
+}
